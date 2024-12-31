289d12bf72e697e77400b02eccbc4855cf94f832
SergioSwede <s.n.o.r.k.00yandex@gmail.com>
2024-12-31 08:10:24 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,15 @@
+def get_matrix(n = 2, m = 2, value = 10):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+result1 = get_matrix()
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+
+print(result1)
+print(result2)
+print(result3)
